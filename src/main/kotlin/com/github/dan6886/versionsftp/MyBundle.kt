061917ad5b513ee569e212cc3381d267c435a9e6
GitHub Action <action@github.com>
2021-11-17 11:23:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/dan6886/versionsftp/MyBundle.kt
+++ src/main/kotlin/com/github/dan6886/versionsftp/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dan6886.versionsftp
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dan6886/versionsftp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dan6886/versionsftp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dan6886.versionsftp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dan6886.versionsftp.MyBundle
 
 class MyApplicationService {
 
